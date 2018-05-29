0a800dd77fa6c1e74094c755d826ece7dac78995
Floyd Kots <floydkotohoyoh@gmail.com>
2018-05-29 15:37:14 +0300

    Basic AMI with nginx installed

---
+++ 1/moduleone.tf
@@ -0,0 +1,45 @@
+### VARIABLES
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "private_key_path" {}
+variable "aws_security_group_id" {}
+variable "key_name" {
+  default = "pluralsight_terraform"
+}
+
+
+### PROVIDERS
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region = "us-east-2"
+}
+
+### RESOURCES
+
+resource "aws_instance" "nginx" {
+  ami = "ami-922914f7"
+  instance_type = "t2.micro"
+  key_name = "${var.key_name}"
+  vpc_security_group_ids = ["${var.aws_security_group_id}"]
+  connection {
+    user = "ec2-user"
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install nginx -y",
+      "sudo service nginx start"
+    ]
+  }
+}
+
+
+### OUTPUT
+
+output "aws_instance_public_dns" {
+  value = "${aws_instance.nginx.public_dns}"
+}
